9c71f00e54bc50535844fc3e9b96bda9485df8e3
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2024-10-20 20:22:46 +0200

    [?] Intermediate state

--- Sequencer/Interface.lua
+++
@@ -1,9 +0,0 @@
--- Interface for converting between key-values and sequences
-
-return
-  {
-    -- Lua table to list tree
-    TableToSeq = request('TableToSeq'),
-    -- List tree to Lua table
-    SeqToTable = request('SeqToTable'),
-  }
