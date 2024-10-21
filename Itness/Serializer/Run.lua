05d724a78bba22f7cc4fc9bd17811f42ba49b0cc
Ian <ian@somewhere.com>
2024-10-21 11:31:48 +0200

    [!^] [DelimitersWriter] Astonishingly simple implementation..
    I've implemented similar decision matrix idea days before in
    my [me_Console] C++ Arduino library.
    
    But in Lua it's a lot more pleasant because of associative arrays and
    first-class functions.

--- Itness/Serializer/Run.lua
+++ Itness/Serializer/Run.lua
@@ -20,52 +20,6 @@ local DataWriter = request('DataWriter.Interface')
 local Indenter = request('DelimitersWriter.Interface')
 
 
--- Get element type
---[[
-  Interface
-
-    (table/string): string (= String, Container)
-]]
-local GetElementType =
-  function(Node)
-    if is_string(Node) then
-      return 'String'
-    elseif is_table(Node) then
-      return 'Container'
-    end
-  end
-
---[[
-  Determine node role.
-
-  If node has more than one entries, we assume it is sequence of
-  key-value elements. So length of table must be even and >= 2.
-
-  (Empty tables are not produced by sequencer and ignored.)
-
-  I can not reduce world to keys and values. Root table. Or wrapped
-  lists. They are neither keys nor values (because they have their pair).
-  So "Object" for case when table has only one entry.
-
-  Interface
-
-    (Index: int): string (= Object, Key, Value)
-]]
-local GetNodeRole =
-  function(Index, NumElements)
-    assert_integer(Index)
-
-    if (NumElements == 1) then
-      return 'Object'
-    end
-
-    if (Index % 2 == 1) then
-      return 'Key'
-    else
-      return 'Value'
-    end
-  end
-
 --[[
   Serialize tree node dispatcher
 
@@ -73,44 +27,27 @@ local GetNodeRole =
 
     Node: string or table
 
-    NodeRole: str: (= Object, Key, Value)
-
   Output
 
     Uses output class.
 ]]
 local Serialize
 Serialize =
-  function(Node, NodeRole)
-    local NodeType = GetElementType(Node)
-
-    local Before = 'Before'
-    local After = 'After'
-
-    if (NodeType == 'String') then
-      local Event = 'Write'
-
-      Indenter:EventNotification(Before, Event, NodeType, NodeRole)
+  function(Node)
+    if is_string(Node) then
+      Indenter:OnEvent('WriteString')
       DataWriter:WriteLeaf(Node)
-      Indenter:EventNotification(After, Event, NodeType, NodeRole)
 
-    elseif (NodeType == 'Container') then
-      local Event = 'StartList'
-
-      Indenter:EventNotification(Before, Event, NodeType, NodeRole)
+    elseif is_table(Node) then
+      Indenter:OnEvent('StartList')
       DataWriter:StartList()
-      Indenter:EventNotification(After, Event, NodeType, NodeRole)
 
       for Index, Entity in ipairs(Node) do
-        local EntityRole = GetNodeRole(Index, #Node)
-        Serialize(Entity, EntityRole)
+        Serialize(Entity)
       end
 
-      local Event = 'EndList'
-
-      Indenter:EventNotification(Before, Event, NodeType, NodeRole)
+      Indenter:OnEvent('EndList')
       DataWriter:EndList()
-      Indenter:EventNotification(After, Event, NodeType, NodeRole)
     end
   end
 
@@ -134,10 +71,10 @@ local SerializeWrapper =
     Indenter.Output = Writer
 
     for Key, Value in ipairs(Tree) do
-      Serialize(Value, 'Object')
+      Serialize(Value)
     end
 
-    Writer:Write('\n')
+    Indenter:OnEvent('Nothing')
   end
 
 -- Exports:
