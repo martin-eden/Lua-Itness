982b4d08f592950478889b647b8a006654752766
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2026-05-26 18:14:53 +0200

    [+] [build.rebuild.sh] Rebuild script.

---
+++ build/rebuild.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Create "workshop/" directory with needed files from current
+# version of "workshop" code hive.
+
+cd ..
+
+rm -rf workshop/
+
+lua build/create_deploy.lua
+
+bash deploy.sh
+rm deploy.sh
+
+mv deploy/workshop .
+rm -rf deploy/
